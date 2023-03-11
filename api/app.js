5598434584025f8be9f82e85952328bbf9bb8b0b
devbxtzz <batztonnie@gmail.com>
2023-03-11 09:30:01 +0300

    Added Routes and Controllers

--- api/app.js
+++ api/app.js
@@ -5,20 +5,43 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require('cors');
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
 
 
+// import routes
+const authRoutes = require('./routes/authRoutes');
 
 //database connection
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-    .then(() => console.log('Connected to MongoDB!!!'))
+    .then(() => console.log("Connected to MongoDB !!!"))
     .catch((err) => console.log(err));
 
+//MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+
+//ROUTES MIDDLEWARE
+// app.get('/', (req, res) => {
+//     res.send("Hello from Node Js");
+// })
+app.use('/api', authRoutes);
+
+// error middleware
+app.use(errorHandler);
 
 //port
-const port = process.env.PORT || 8001
+const port = process.env.PORT || 9000
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
